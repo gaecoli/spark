126cc0b6409c89943900b8fe7052f9793637b247
报时 <baoshi@fordeal.com>
2022-08-24 18:33:56 +0800

    submit should always output application information
    # Conflicts:
    #	resource-managers/kubernetes/core/src/main/scala/org/apache/spark/deploy/k8s/submit/KubernetesClientApplication.scala

--- resource-managers/kubernetes/core/src/main/scala/org/apache/spark/deploy/k8s/submit/KubernetesClientApplication.scala
+++ resource-managers/kubernetes/core/src/main/scala/org/apache/spark/deploy/k8s/submit/KubernetesClientApplication.scala
@@ -180,26 +180,23 @@ private[spark] class Client(
         throw e
     }
 
-    val sId = Client.submissionId(conf.namespace, driverPodName)
-    if (conf.get(WAIT_FOR_APP_COMPLETION)) {
-      breakable {
-        while (true) {
-          val podWithName = kubernetesClient
-            .pods()
-            .inNamespace(conf.namespace)
-            .withName(driverPodName)
-          // Reset resource to old before we start the watch, this is important for race conditions
-          watcher.reset()
-          watch = podWithName.watch(watcher)
+    val sId = Seq(conf.namespace, driverPodName).mkString(":")
+    breakable {
+      while (true) {
+        val podWithName = kubernetesClient
+          .pods()
+          .withName(driverPodName)
+        // Reset resource to old before we start the watch, this is important for race conditions
+        watcher.reset()
+        watch = podWithName.watch(watcher)
 
-          // Send the latest pod state we know to the watcher to make sure we didn't miss anything
-          watcher.eventReceived(Action.MODIFIED, podWithName.get())
+        // Send the latest pod state we know to the watcher to make sure we didn't miss anything
+        watcher.eventReceived(Action.MODIFIED, podWithName.get())
 
-          // Break the while loop if the pod is completed or we don't want to wait
-          if (watcher.watchOrStop(sId)) {
-            watch.close()
-            break
-          }
+        // Break the while loop if the pod is completed or we don't want to wait
+        if (watcher.watchOrStop(sId)) {
+          watch.close()
+          break
         }
       }
     } else {
